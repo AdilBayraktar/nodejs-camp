054d6717fca87fea0c45501d2ec185e62af7bee9
AdilBayraktar <adilbayraktar1997@gmail.com>
2022-12-14 12:58:41 +0300

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solving(a, b) {
+  console.log("Solving: a = " + a + " & b = " + b);
+  if (a <= 0 || b <= 0) {
+    console.log("Values must be greater than Zero");
+  } else {
+    console.log("Area = " + rect.area(a, b));
+    console.log("Perimeter = " + rect.perimeter(a, b));
+  }
+}
+
+solving(2, 8);
+solving(0, 16);
